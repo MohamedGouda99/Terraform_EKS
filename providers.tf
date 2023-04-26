8bf4110ab355e4caccf04fd858228c880d69973a
MohamedGouda99 <ma1582830@gmail.com>
2023-04-26 06:05:12 +0200

    deploy-4

--- providers.tf
+++ providers.tf
@@ -17,9 +17,20 @@ provider "kubernetes" {
 }
 
 
-provider "aws" {
-  region  = "us-west-2"
-  version = "~> 3.73"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.73"
+    }
+  }
 }
 
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+#
 
