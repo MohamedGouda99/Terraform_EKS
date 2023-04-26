1b400ff61d08f1038a133230162820934986d2b2
MohamedGouda99 <ma1582830@gmail.com>
2023-04-26 06:08:00 +0200

    deploy-4

--- providers.tf
+++ providers.tf
@@ -22,15 +22,12 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.73"
+
     }
   }
 }
 
-provider "aws" {
-  region     = "us-west-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
 
-#
+
+
 
